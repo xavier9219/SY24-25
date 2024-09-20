edb001c83c92c47421487d9ed540b56f48fc97fb
xavier sicairos <xasic937@learner.dvusd.org>
2024-09-20 12:38:56 -0700

    first version of game

---
+++ tetris/tetris/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        avatar = New PictureBox()
+        Timer1 = New Timer(components)
+        astroid = New PictureBox()
+        enemy = New PictureBox()
+        enemy2 = New PictureBox()
+        CType(avatar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(astroid, ComponentModel.ISupportInitialize).BeginInit()
+        CType(enemy, ComponentModel.ISupportInitialize).BeginInit()
+        CType(enemy2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' avatar
+        ' 
+        avatar.Image = My.Resources.Resources._439_4392668_pixel_spaceship_png_pixel_spaceship
+        avatar.Location = New Point(253, 12)
+        avatar.Name = "avatar"
+        avatar.Size = New Size(86, 88)
+        avatar.SizeMode = PictureBoxSizeMode.Zoom
+        avatar.TabIndex = 0
+        avatar.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' astroid
+        ' 
+        astroid.Image = My.Resources.Resources._3df0c312d89ed5b_pngastroid
+        astroid.Location = New Point(424, 194)
+        astroid.Name = "astroid"
+        astroid.Size = New Size(130, 130)
+        astroid.SizeMode = PictureBoxSizeMode.Zoom
+        astroid.TabIndex = 1
+        astroid.TabStop = False
+        ' 
+        ' enemy
+        ' 
+        enemy.Image = My.Resources.Resources._363116e7da047bd
+        enemy.Location = New Point(100, 115)
+        enemy.Name = "enemy"
+        enemy.Size = New Size(181, 112)
+        enemy.SizeMode = PictureBoxSizeMode.Zoom
+        enemy.TabIndex = 2
+        enemy.TabStop = False
+        ' 
+        ' enemy2
+        ' 
+        enemy2.Image = My.Resources.Resources._363116e7da047bd
+        enemy2.Location = New Point(663, 319)
+        enemy2.Name = "enemy2"
+        enemy2.Size = New Size(181, 112)
+        enemy2.SizeMode = PictureBoxSizeMode.Zoom
+        enemy2.TabIndex = 3
+        enemy2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1507, 750)
+        Controls.Add(enemy2)
+        Controls.Add(enemy)
+        Controls.Add(astroid)
+        Controls.Add(avatar)
+        Name = "Form1"
+        Text = "Form1"
+        CType(avatar, ComponentModel.ISupportInitialize).EndInit()
+        CType(astroid, ComponentModel.ISupportInitialize).EndInit()
+        CType(enemy, ComponentModel.ISupportInitialize).EndInit()
+        CType(enemy2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents avatar As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents astroid As PictureBox
+    Friend WithEvents enemy As PictureBox
+    Friend WithEvents enemy2 As PictureBox
+
+End Class
---
+++ tetris/tetris/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        follow(enemy, avatar, 7)
+        follow(enemy2, avatar, 5)
+    End Sub
+    Sub follow(e As PictureBox, a As PictureBox, s As Integer)
+        If e.Location.Y > a.Location.Y Then
+            move(e, 0, -s)
+        Else
+            move(e, 0, s)
+        End If
+        If e.Location.X > a.Location.X Then
+            move(e, -s, 0)
+        Else
+            move(e, s, 0)
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Left Or e.KeyCode = Keys.A Then
+            move(avatar, -5, 0)
+        End If
+        If e.KeyCode = Keys.Right Or e.KeyCode = Keys.D Then
+            move(avatar, 5, 0)
+        End If
+        If e.KeyCode = Keys.Down Or e.KeyCode = Keys.S Then
+            move(avatar, 0, 5)
+        End If
+        If e.KeyCode = Keys.Space Or e.KeyCode = Keys.Space Then
+            avatar.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+        End If
+        avatar.Refresh()
+        If e.KeyCode = Keys.Up Or e.KeyCode = Keys.W Then
+            move(avatar, 0, -5)
+        End If
+        avatar.Refresh()
+    End Sub
+    Sub move(p As PictureBox, xdir As Integer, ydir As Integer)
+        p.Location += New Point(xdir, ydir)
+        If p.Bounds.IntersectsWith(astroid.Bounds) Then
+            astroid.Visible = False
+        End If
+    End Sub
+End Class
