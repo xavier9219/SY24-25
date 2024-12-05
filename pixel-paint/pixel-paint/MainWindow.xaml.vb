337ab0b500e74c9c13d9aa5f1d902ea56400101c
xavier sicairos <xasic937@learner.dvusd.org>
2024-12-05 14:09:51 -0700

    paint

---
+++ pixel-paint/pixel-paint/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub fill1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles fill1.MouseDown, fill2.MouseDown, fill3.MouseDown, fill4.MouseDown, fill5.MouseDown, fill6.MouseDown, fill7.MouseDown, fill8.MouseDown
+        brush1.Fill = sender.fill
+    End Sub
+End Class
---
+++ wpf.dex/wpf.dex/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Santa.Visibility = Visibility.Visible
+        Baseball.Visibility = Visibility.Hidden
+        Mariners.Visibility = Visibility.Hidden
+        Info.Content = "Santa is delivering presents, he is on a roof, he has 3 presents,he has no sliegh."
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        Santa.Visibility = Visibility.Hidden
+        Baseball.Visibility = Visibility.Visible
+        Mariners.Visibility = Visibility.Hidden
+        Info.Content = "the baseball is clean, it has 216 laces, it weighs 5.25 oz, and it has red laces."
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        Santa.Visibility = Visibility.Hidden
+        Baseball.Visibility = Visibility.Hidden
+        Mariners.Visibility = Visibility.Visible
+        Info.Content = "The Seattle Mariners have been part of Major League Baseball since 1977.
+2.The Mariners have never won a World Series.
+3.The Mariners have the record for longest time before posting a winning season.
+4.The Mariners' home stadium is T-Mobile Park."
+    End Sub
+End Class
